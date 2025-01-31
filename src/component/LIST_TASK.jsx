e00b98fdc455cddb17b5c3254b82e27635e20911
tanvi-rathod-techno <138793969+tanvi-rathod-techno@users.noreply.github.com>
2025-01-31 16:52:30 +0530

    Filter added.

--- src/component/LIST_TASK.jsx
+++ src/component/LIST_TASK.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeTask } from '../redux/addTask';
 import { toggleTask } from '../redux/addTask';
@@ -6,6 +6,7 @@ import { removeCompetedTask } from '../redux/addTask';
 
 function List_Task() {
     const tasks = useSelector(state => state.tasks);
+    const [filter,setFilter] = useState('all');
     const dispatch = useDispatch();
 
     const handelremove = (id) => {
@@ -20,11 +21,29 @@ function List_Task() {
         dispatch(removeCompetedTask());
     }
 
+    
+    const handelfilter = tasks.filter((task) =>{
+        if (filter === 'completed') return task.completed;
+        if (filter === 'active') return !task.completed;
+        return true;
+    }) 
+
     return (
             <div className="max-w-lg mx-auto mt-6 p-4 bg-white shadow-md rounded-lg">
+            <div className="flex gap-4 mb-4">
+                <button onClick={() => setFilter('all')} className="px-4 py-2 bg-blue-500 text-white rounded">
+                    All
+                </button>
+                <button onClick={() => setFilter('active')} className="px-4 py-2 bg-yellow-500 text-white rounded">
+                    Active
+                </button>
+                <button onClick={() => setFilter('completed')} className="px-4 py-2 bg-green-500 text-white rounded">
+                    Completed
+                </button>
+            </div>
                 <ul className="space-y-2">
                     {
-                        tasks.map(task => {
+                        handelfilter.map(task => {
                             return <li
                                 key={task.id}
                                 className={`flex justify-between items-center p-2 border-b rounded-md ${task.completed ? "bg-green-100  text-gray-500" : "bg-gray-100"
